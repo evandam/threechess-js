47846f9a5bae0cd6f9ea8519bd239cd3366acca2
Daniel Rogers <djamarr@gmail.com>
2013-11-17 20:11:37 -0500

    Set up movement function Added pieces to a location map so that the movement function can reference them and make changes to location Gave pieces their own move functions   - Can be extended later to make fancier movements Added test code to controller to check movement capabilities Lightened black pieces slightly so they are easier to distinguish

--- js/engine/board.js
+++ js/engine/board.js
@@ -3,10 +3,12 @@
  * 
  * @param scene
  *            The scene that this board will attach to
+ * @param afterload
+ *            The function to execute after the board is loaded
  */
-function Board( scene ) {
+function Board( scene, afterload ) {
     // No generated pieces yet
-    this.pieces = [ ];
+    this.pieces = { };
 
     // The 3D board object, Board intial load code by @evan
     this.board = new THREE.Object3D();
@@ -36,7 +38,7 @@ function Board( scene ) {
         board.add(cloned);
 
         // Now that the board layout is done, put the pieces on it
-        self.loadPieces();
+        self.loadPieces(afterload);
     });
 
     // Add it to the passed-in scene
@@ -44,12 +46,73 @@ function Board( scene ) {
 };
 
 /**
- * Adds pieces to the board for display
+ * Moves a piece from a position on the board to a new position
+ * 
+ * @param start
+ *            The start position of the piece in the form "a7" etc.
+ * @param end
+ *            The end position of the piece
  */
-Board.prototype.loadPieces = function( ) {
+Board.prototype.movePiece = function( start, end ) {
+
+    if ( start in this.pieces ) {
+
+        // Map of chars to numbers for get dy
+        // maybe could be cleaned up later
+        var map =
+        {
+            'a' : 1,
+            'b' : 2,
+            'c' : 3,
+            'd' : 4,
+            'e' : 5,
+            'f' : 6,
+            'g' : 7,
+            'h' : 8
+        };
+
+        var getDZ = function( s, e ) {
+            return -( e - s );
+        };
+
+        var getDX = function( s, e ) {
+            return map[e] - map[s];
+        };
+
+        // Get the piece from the piece map
+        var piece = this.pieces[start];
+        piece.move(getDX(start[0], end[0]), getDZ(start[1], end[1]));
+        // TODO Handle captures/pieces landing on same square
+        // Remove the old piece @ old position
+        delete this.pieces[start];
+        // Put it in the new position
+        this.pieces[end] = piece;
+    }
+    else {
+        console.log("No piece @ " + start);
+        console.log(this.pieces);
+    }
+};
+
+/**
+ * Adds pieces to the board for display
+ * 
+ * @param afterload
+ *            The function to execute after loading is complete
+ */
+Board.prototype.loadPieces = function( afterload ) {
     // Get an object loader
     var loader = new THREE.OBJMTLLoader();
     var board = this.board;
+    var self = this;
+
+    var pieceCount = 0;
+
+    var checkLoaded = function( ) {
+        // If everything is loaded, done
+        if ( pieceCount == 32 )
+            afterload();
+    };
 
     var setWhite = function( object ) {
         object.traverse(function( child ) {
@@ -61,7 +124,7 @@ Board.prototype.loadPieces = function( ) {
     var setBlack = function( object ) {
         object.traverse(function( child ) {
             if ( child instanceof THREE.Mesh )
-                child.material.color.setRGB(0, 0, 0);
+                child.material.color.setRGB(.10, .10, .10);
         });
     };
 
@@ -71,19 +134,35 @@ Board.prototype.loadPieces = function( ) {
         piece.scale.z = 0.50;
     };
 
+    var map = [ "a", "b", "c", "d", "e", "f", "g", "h" ];
+
     /*
      * Pawn-specific loading functions
      */
-    var whiteInitial = function( piece ) {
+    var whiteInitial = function( piece, pos ) {
         piece.translateX(-3.5);
         piece.translateY(1);
         piece.translateZ(2.5);
+        // White starts at a2
+        pos[0] = 'a';
+        pos[1] = 2;
     };
 
-    var blackInitial = function( piece ) {
+    var blackInitial = function( piece, pos ) {
         piece.translateX(-3.5);
         piece.translateY(1);
         piece.translateZ(-2.5);
+        // Black starts at a7
+        pos[0] = 'a';
+        pos[1] = 7;
+    };
+
+    // Pawn movement function
+    var simpleMove = function( dx, dz ) {
+        if ( dx )
+            this.translateX(dx);
+        if ( dz )
+            this.translateZ(dz);
     };
 
     /*
@@ -94,21 +173,29 @@ Board.prototype.loadPieces = function( ) {
         loader.load('models/pawn.obj', 'models/pawn.mtl', function( object ) {
             // Make the pawns
             var pawn = object;
+            var pos = [ ];
 
             // Set up the first one
             setcolor(pawn);
             scalePiece(pawn);
 
             // Move into position
-            startpos(pawn);
+            startpos(pawn, pos);
+            // Move function for later
+            pawn.move = simpleMove;
 
             // Add to the board
             board.add(pawn);
+            // Store it so it can be moved
+            self.pieces[pos.join('')] = pawn;
+            pieceCount++;
 
             // Place the rest
             for ( var i = 1; i < 8; ++i ) {
+                pos = [ map[i], pos[1] ];
                 // Clone it
                 pawn = pawn.clone();
+                pawn.move = move;
 
                 // Move it into place
                 // Should probably be replaced by a call to the moveTo function
@@ -117,7 +204,11 @@ Board.prototype.loadPieces = function( ) {
 
                 // Add it to the board
                 board.add(pawn);
+                self.pieces[pos.join('')] = pawn;
+                pieceCount++;
             }
+
+            checkLoaded();
         });
     };
 
@@ -131,16 +222,22 @@ Board.prototype.loadPieces = function( ) {
     /*
      * Rook loading functions
      */
-    whiteInitial = function( piece ) {
+    whiteInitial = function( piece, pos ) {
         piece.translateX(-3.5);
         piece.translateY(1);
         piece.translateZ(3.5);
+        // white starts at a1
+        pos[0] = 'a';
+        pos[1] = 1;
     };
 
-    blackInitial = function( piece ) {
+    blackInitial = function( piece, pos ) {
         piece.translateX(-3.5);
         piece.translateY(1);
         piece.translateZ(-3.5);
+        // black starts at a8
+        pos[0] = 'a';
+        pos[1] = 8;
     };
 
     /*
@@ -149,22 +246,34 @@ Board.prototype.loadPieces = function( ) {
     var loadRooks = function( setcolor, startpos ) {
         loader.load('models/rook.obj', 'models/rook.mtl', function( object ) {
             var rook = object;
+            rook.move = simpleMove;
+            var pos = [ ];
+
             // Color and put in first position
             setcolor(rook);
-            startpos(rook);
+            startpos(rook, pos);
 
             // Scale it appropriately
             scalePiece(rook);
 
             // Add it to the board
             board.add(rook);
+            self.pieces[pos.join('')] = rook;
+            pieceCount++;
 
             // Make the other rook
             rook = rook.clone();
+            rook.move = move;
             // Move to other side of board
             rook.translateX(7);
+            // h1 or h8
+            pos[0] = 'h';
             // Add it to the board
             board.add(rook);
+            self.pieces[pos.join('')] = rook;
+            pieceCount++;
+
+            checkLoaded();
         });
     };
 
@@ -177,16 +286,21 @@ Board.prototype.loadPieces = function( ) {
     /*
      * Knight loading functions
      */
-    whiteInitial = function( piece ) {
+    whiteInitial = function( piece, pos ) {
         piece.translateX(-2.5);
         piece.translateY(1);
         piece.translateZ(3.5);
+        // b1
+        pos[0] = 'b';
+        pos[1] = 1;
     };
 
-    blackInitial = function( piece ) {
+    blackInitial = function( piece, pos ) {
         piece.translateX(-2.5);
         piece.translateY(1);
         piece.translateZ(-3.5);
+        pos[0] = 'b';
+        pos[1] = 8;
     };
 
     var whiteRotation = function( piece ) {
@@ -197,6 +311,28 @@ Board.prototype.loadPieces = function( ) {
         piece.rotateY(-Math.PI);
     };
 
+    var kMove = function( dx, dz ) {
+        // Handle the fact that knights have to rotate
+        if ( this.rotate ) {
+            // reset
+            this.resetRotation(this);
+            // move
+            if ( dx )
+                this.translateX(dx);
+            if ( dz )
+                this.translateZ(dz);
+            // re-rotate
+            this.rotate(this);
+        }
+        else {
+            // Just move
+            if ( dx )
+                this.translateX(dx);
+            if ( dz )
+                this.translateZ(dz);
+        }
+    };
+
     /*
      * Object load function for the knights
      */
@@ -204,27 +340,43 @@ Board.prototype.loadPieces = function( ) {
         loader.load('models/knight.obj', 'models/knight.mtl',
                 function( object ) {
                     var knight = object;
+                    var pos = [ ];
+                    knight.move = kMove;
+                    knight.rotate = dorotation;
+                    knight.resetRotation = resetrotation;
 
                     // You know the drill
                     setcolor(knight);
-                    startpos(knight);
+                    startpos(knight, pos);
                     scalePiece(knight);
                     board.add(knight);
+                    self.pieces[pos.join('')] = knight;
+                    pieceCount++;
 
                     if ( dorotation )
                         dorotation(knight);
 
                     // and the second
                     knight = knight.clone();
+                    knight.move = kMove;
+                    knight.rotate = dorotation;
+                    knight.resetRotation = resetrotation;
+
                     // Clear the rotation for movement
                     if ( resetrotation )
                         resetrotation(knight);
                     // Move into position
                     knight.translateX(5);
+                    // g1 or g8
+                    pos[0] = 'g';
                     // Do the rotation
                     if ( dorotation )
                         dorotation(knight);
                     board.add(knight);
+                    self.pieces[pos.join('')] = knight;
+                    pieceCount++;
+
+                    checkLoaded();
                 });
     };
 
@@ -237,16 +389,20 @@ Board.prototype.loadPieces = function( ) {
     /*
      * Bishop loading functions
      */
-    whiteInitial = function( piece ) {
+    whiteInitial = function( piece, pos ) {
         piece.translateX(-1.5);
         piece.translateY(1);
         piece.translateZ(3.5);
+        pos[0] = 'c';
+        pos[1] = 1;
     };
 
-    blackInitial = function( piece ) {
+    blackInitial = function( piece, pos ) {
         piece.translateX(-1.5);
         piece.translateY(1);
         piece.translateZ(-3.5);
+        pos[0] = 'c';
+        pos[1] = 8;
     };
 
     whiteRotation = function( piece ) {
@@ -267,6 +423,19 @@ Board.prototype.loadPieces = function( ) {
         piece.rotateY(-( Math.PI * 1.5 ));
     };
 
+    var bMove = function( dx, dz ) {
+        // Handle the fact that bishops have to rotate
+        // reset
+        this.resetRotation(this);
+        // move
+        if ( dx )
+            this.translateX(dx);
+        if ( dz )
+            this.translateZ(dz);
+        // re-rotate
+        this.rotate(this);
+    };
+
     /*
      * Object load function for the bishops
      */
@@ -274,21 +443,39 @@ Board.prototype.loadPieces = function( ) {
         loader.load('models/bishop.obj', 'models/bishop.mtl',
                 function( object ) {
                     var bishop = object;
+                    var pos = [ ];
+                    
+                    bishop.move = bMove;
+                    bishop.rotate = dorotation;
+                    bishop.resetRotation = resetrotation;
 
                     // You know the drill
                     setcolor(bishop);
-                    startpos(bishop);
+                    startpos(bishop, pos);
                     scalePiece(bishop);
                     dorotation(bishop);
 
                     board.add(bishop);
+                    self.pieces[pos.join('')] = bishop;
+                    pieceCount++;
 
                     // and the second
                     bishop = bishop.clone();
+                    bishop.move = bMove;
+                    bishop.rotate = dorotation;
+                    bishop.resetRotation = resetrotation;
+
                     resetrotation(bishop);
                     bishop.translateX(3);
+                    // f1 or f8
+                    pos[0] = 'f';
+
                     dorotation(bishop);
                     board.add(bishop);
+                    self.pieces[pos.join('')] = bishop;
+                    pieceCount++;
+
+                    checkLoaded();
                 });
     };
 
@@ -301,16 +488,27 @@ Board.prototype.loadPieces = function( ) {
     /*
      * Queen loading functions
      */
-    whiteInitial = function( piece ) {
+    whiteInitial = function( piece, pos ) {
         piece.translateX(-0.5);
         piece.translateY(1);
         piece.translateZ(3.5);
+        pos[0] = 'd';
+        pos[1] = 1;
     };
 
-    blackInitial = function( piece ) {
+    blackInitial = function( piece, pos ) {
         piece.translateX(-0.5);
         piece.translateY(1);
         piece.translateZ(-3.5);
+        pos[0] = 'd';
+        pos[1] = 8;
+    };
+
+    var move = function( dx, dz ) {
+        if ( dx )
+            this.translateX(dx);
+        if ( dz )
+            this.translateZ(dz);
     };
 
     /*
@@ -319,12 +517,20 @@ Board.prototype.loadPieces = function( ) {
     var loadQueen = function( setcolor, startpos ) {
         loader.load('models/queen.obj', 'models/queen.mtl', function( object ) {
             var queen = object;
+            var pos = [ ];
+            
+            queen.move = move;
+
             // Only one king
             setcolor(queen);
-            startpos(queen);
+            startpos(queen, pos);
             scalePiece(queen);
 
             board.add(queen);
+            self.pieces[pos.join('')] = queen;
+            pieceCount++;
+
+            checkLoaded();
         });
     };
 
@@ -337,16 +543,20 @@ Board.prototype.loadPieces = function( ) {
     /*
      * King loading functions
      */
-    whiteInitial = function( piece ) {
+    whiteInitial = function( piece, pos ) {
         piece.translateX(0.5);
         piece.translateY(1);
         piece.translateZ(3.5);
+        pos[0] = 'e';
+        pos[1] = 1;
     };
 
-    blackInitial = function( piece ) {
+    blackInitial = function( piece, pos ) {
         piece.translateX(0.5);
         piece.translateY(1);
         piece.translateZ(-3.5);
+        pos[0] = 'e';
+        pos[1] = 8;
     };
 
     /*
@@ -355,12 +565,20 @@ Board.prototype.loadPieces = function( ) {
     var loadKing = function( setcolor, startpos ) {
         loader.load('models/king.obj', 'models/king.mtl', function( object ) {
             var king = object;
+            var pos = [ ];
+            
+            king.move = move;
+
             // Only one king
             setcolor(king);
-            startpos(king);
+            startpos(king, pos);
             scalePiece(king);
 
             board.add(king);
+            self.pieces[pos.join('')] = king;
+            pieceCount++;
+
+            checkLoaded();
         });
     };
 
--- js/engine/controller.js
+++ js/engine/controller.js
@@ -4,8 +4,13 @@
  */
 function Controller( scene ) {
     // Get a new chess board with all the pieces
-    //  Hold a reference to it so it can be manipulated later
-    this.board = new Board(scene);
+    // Hold a reference to it so it can be manipulated later
+    var self = this;
+    this.board = new Board(scene, function( ) {
+        setTimeout(function( ) {
+            self.connectTo();
+        }, 2000);
+    });
 };
 
 /**
@@ -23,5 +28,34 @@ Controller.prototype.connectTo = function( url ) {
     }
     else {
         // TODO Set up debug interactions
+        // Check some basic movement of pawns
+        // white forward 1
+        this.board.movePiece('a2', 'a3');
+        this.board.movePiece('d2', 'd3');
+        // white diagonal
+        this.board.movePiece('b2', 'c3');
+        // black forward 2
+        this.board.movePiece('d7', 'd5');
+        //Then rooks
+        //white/black forward 3
+        this.board.movePiece('a1', 'a4');
+        this.board.movePiece('h8', 'h4');
+        //Then knights, white
+        this.board.movePiece('g1', 'h3');
+        this.board.movePiece('b1', 'd2');
+        //and black
+        this.board.movePiece('g8', 'f6');
+        //Then bishop, 2 diagonal white/black
+        this.board.movePiece('c1', 'e3');
+        this.board.movePiece('f8', 'd6');
+        //Then queen
+        this.board.movePiece('d1', 'a1');
+        this.board.movePiece('d8', 'h8');
+        //Reverse it to check that we're updating the map
+        this.board.movePiece('a1', 'b1');
+        this.board.movePiece('h8', 'g8');
+        //Then king
+        this.board.movePiece('e1', 'd1');
+        this.board.movePiece('e8', 'd8');
     }
 };
